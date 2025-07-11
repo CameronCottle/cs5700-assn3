c394b0f400c9166d80289ff6d6ab0e3cb69c47a7
ccottle <ramm@gmail.com>
2025-07-10 22:52:22 -0400

    set up environment for implementation

---
+++ composeApp/src/desktopMain/kotlin/org/example/project/App.kt
@@ -0,0 +1,49 @@
+package org.example.project
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key.Companion.Home
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import assn2.composeapp.generated.resources.Res
+import assn2.composeapp.generated.resources.compose_multiplatform
+import org.example.project.screens.Home
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+       val navController = rememberNavController()
+        val navigation = remember{ComposeNavigation(navController)}
+//        LaunchedEffect(Unit){
+//            navigation.goToSettings()
+//        }
+        NavHost(
+            navController,
+            startDestination = Destinations.Home
+        ) {
+            composable<Destinations.Home> {
+                Home(navigation)
+            }
+
+
+            composable<Destinations.Settings> {
+                Settings(goBack = {navController.popBackStack()})
+            }
+        }
+    }
+}
---
+++ composeApp/src/desktopMain/kotlin/org/example/project/screens/Home.kt
@@ -0,0 +1,21 @@
+package org.example.project.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import org.example.project.Destinations
+import org.example.project.Navigation
+
+@Composable
+fun Home (navigation: Navigation) {
+
+    Column {
+        Text("Home Page")
+        Button(onClick = {
+            navigation.goToSettings()
+        }) {
+            Text("Go to Settings")
+        }
+    }
+}
